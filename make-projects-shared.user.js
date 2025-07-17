e182e40f037117a8b167bae70bdcbab8d90a0ee0
Wiktor Laskowski <117634264+wiktorlaskowski@users.noreply.github.com>
2025-07-17 20:13:45 +0200

    Fix compatibility issues

--- make-projects-shared.user.js
+++ make-projects-shared.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Make Scratch Projects Shared
 // @description Allows you to view any Scratch project as if it was shared.
-// @version 1.1.3
+// @version 1.2
 // @namespace https://github.com/forkphorus/cat-plus
 // @homepageURL https://github.com/forkphorus/cat-plus#readme
 // @match https://scratch.mit.edu/projects/*
@@ -13,75 +13,99 @@
 
   var magic = '#_bypasssharerestrictions_';
 
+  function buildData(id) {
+    var time = Date.now() / 1000 | 0;
+    return {
+      id: id,
+      title: '(unshared project)',
+      description: 'Description is unavailable.',
+      instructions: '',
+      visibility: 'visible',
+      public: true,
+      is_published: true,
+      comments_allowed: true,
+      author: {
+        id: 1,
+        username: 'unknown',
+        scratchteam: false,
+        history: {
+          joined: '1900-01-01T00:00:00.000Z',
+        },
+        profile: {
+          id: null,
+          images: {
+            '90x90': 'https://cdn2.scratch.mit.edu/get_image/user/default_90x90.png?v=',
+            '60x60': 'https://cdn2.scratch.mit.edu/get_image/user/default_60x60.png?v=',
+            '55x55': 'https://cdn2.scratch.mit.edu/get_image/user/default_55x55.png?v=',
+            '50x50': 'https://cdn2.scratch.mit.edu/get_image/user/default_50x50.png?v=',
+            '32x32': 'https://cdn2.scratch.mit.edu/get_image/user/default_32x32.png?v=',
+          },
+        },
+      },
+      image: 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_480x360.png',
+      images: {
+        '282x218': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_282x218.png?v=' + time,
+        '216x163': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_216x163.png?v=' + time,
+        '200x200': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_200x200.png?v=' + time,
+        '144x108': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_144x108.png?v=' + time,
+        '135x102': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_135x102.png?v=' + time,
+        '100x80': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_100x80.png?v=' + time,
+      },
+      history: {
+        created: '1900-01-01T00:00:00.000Z',
+        modified: '1900-01-01T00:00:00.000Z',
+        shared: '1900-01-01T00:00:00.000Z',
+      },
+      stats: {
+        views: 0,
+        loves: 0,
+        favorites: 0,
+        comments: 0,
+        remixes: 0,
+      },
+      remix: {
+        parent: null,
+        root: null,
+      },
+    };
+  }
+
   function patch() {
-    var nativeMethod = unsafeWindow.XMLHttpRequest.prototype.open;
-    unsafeWindow.XMLHttpRequest.prototype.open = function(method, url) {
+    var page = typeof unsafeWindow === 'undefined' ? window : unsafeWindow;
+
+    var nativeXHR = page.XMLHttpRequest.prototype.open;
+    page.XMLHttpRequest.prototype.open = function(method, url) {
       if (!method || !url) {
-        return nativeMethod.apply(this, arguments);
+        return nativeXHR.apply(this, arguments);
       }
-      var re = /^https:\/\/api\.scratch\.mit\.edu\/projects\/(\d+)$/;
+      var re = /^https:\/\/api\.scratch\.mit\.edu\/projects\/(\d+)/;
       var match = url.match(re);
       if (match) {
         var id = match[1];
-        var time = Date.now() / 1000 | 0;
-        // Replace the URL with our own data: URI that has some dummy data
-        // See: https://api.scratch.mit.edu/projects/285708713
-        arguments[1] = 'data:application/json;base64,' + btoa(JSON.stringify({
-          id: id,
-          title: '(unshared project)',
-          description: 'Description is unavailable.',
-          instructions: '',
-          visibility: 'visible',
-          public: true,
-          is_published: true,
-          comments_allowed: true,
-          author: {
-            id: 1,
-            username: 'unknown',
-            scratchteam: false,
-            history: {
-              joined: '1900-01-01T00:00:00.000Z',
-            },
-            profile: {
-              id: null,
-              images: {
-                '90x90': 'https://cdn2.scratch.mit.edu/get_image/user/default_90x90.png?v=',
-                '60x60': 'https://cdn2.scratch.mit.edu/get_image/user/default_60x60.png?v=',
-                '55x55': 'https://cdn2.scratch.mit.edu/get_image/user/default_55x55.png?v=',
-                '50x50': 'https://cdn2.scratch.mit.edu/get_image/user/default_50x50.png?v=',
-                '32x32': 'https://cdn2.scratch.mit.edu/get_image/user/default_32x32.png?v=',
-              },
-            },
-          },
-          image: 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_480x360.png',
-          images: {
-            '282x218': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_282x218.png?v=' + time,
-            '216x163': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_216x163.png?v=' + time,
-            '200x200': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_200x200.png?v=' + time,
-            '144x108': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_144x108.png?v=' + time,
-            '135x102': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_135x102.png?v=' + time,
-            '100x80': 'https://cdn2.scratch.mit.edu/get_image/project/' + id + '_100x80.png?v=' + time,
-          },
-          history: {
-            created: '1900-01-01T00:00:00.000Z',
-            modified: '1900-01-01T00:00:00.000Z',
-            shared: '1900-01-01T00:00:00.000Z',
-          },
-          stats: {
-            views: 0,
-            loves: 0,
-            favorites: 0,
-            comments: 0,
-            remixes: 0,
-          },
-          remix: {
-            parent: null,
-            root: null,
-          },
-        }));
+        var data = buildData(id);
+        arguments[1] = 'data:application/json;base64,' + btoa(JSON.stringify(data));
       }
-      return nativeMethod.apply(this, arguments);
+      return nativeXHR.apply(this, arguments);
     };
+
+    if (page.fetch) {
+      var nativeFetch = page.fetch;
+      page.fetch = function(resource, init) {
+        var url = typeof resource === 'string' ? resource : resource.url;
+        var match = url && url.match(/^https:\/\/api\.scratch\.mit\.edu\/projects\/(\d+)/);
+        if (match) {
+          var id = match[1];
+          var data = buildData(id);
+          var json = JSON.stringify(data);
+          var response = new Response(json, {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+          });
+          return Promise.resolve(response);
+        }
+        return nativeFetch.call(this, resource, init);
+      };
+    }
 
     // Prevent visiting the user page of "unknown" because we use that but its a real user
     window.addEventListener('load', function(e) {
